05acad51ab89092f002a2f07f51b9b90532dbe32
Tomas Orrego <tom.orrego@gmail.com>
2021-08-31 11:20:23 -0400

    cambios nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,37 @@
 let brand = document.getElementById("brand");
 let lineItem = Array.from(document.querySelectorAll('.nav-link'));
 let navbar = document.getElementById("navbar");
+let navToggler = document.getElementById("nav-toggler");
+let wWidth = window.innerWidth;
 
-window.addEventListener('scroll', function () {
-    scrollPosition = window.scrollY;
+function navAdapt() {
+    console.log(wWidth);
+    if (wWidth >= 768) {
+        window.addEventListener('scroll', function () {
+            scrollPosition = window.scrollY;
+            if (scrollPosition >= 30) {
+                navbar.classList.add("scrolled");
+                brand.src = "src/img/logo.svg";
+                lineItem.forEach((arrayElement, index) => {
+                    setTimeout(function () {
+                        lineItem[index].classList.add("text-grey");
+                        lineItem[index].classList.remove("text-white");
+                    }, 20);
+                });
 
-    if (scrollPosition >= 30) {
-        navbar.classList.add("scrolled");
+            } else {
+                navbar.classList.remove("scrolled");
+                brand.src = "src/img/logo-white.svg";
+                lineItem.forEach((arrayElement, index) => {
+                    setTimeout(function () {
+                        lineItem[index].classList.add("text-white");
+                        lineItem[index].classList.remove("text-grey");
+                    }, 20);
+                });
+            }
+
+        });
+    } else {
         brand.src = "src/img/logo.svg";
         lineItem.forEach((arrayElement, index) => {
             setTimeout(function () {
@@ -14,17 +39,21 @@ window.addEventListener('scroll', function () {
                 lineItem[index].classList.remove("text-white");
             }, 20);
         });
-
-    } else if(scrollPosition >= 100){
-        navbar.classList.add("d-none");
-    } else {
-        navbar.classList.remove("scrolled");
-        brand.src = "src/img/logo-white.svg";
-        lineItem.forEach((arrayElement, index) => {
-            setTimeout(function () {
-                lineItem[index].classList.add("text-white");
-                lineItem[index].classList.remove("text-grey");
-            }, 20);
-        });
     }
+}
+
+function printWWidth(){
+    console.log(wWidth);
+}
+window.addEventListener('DOMContentLoaded', ()=>{
+    printWWidth();
+    navAdapt();
+})
+
+/* switch nav styles on scroll */
+window.addEventListener('resize', function () {
+    wWidth = window.innerWidth;
+    printWWidth();
+    navAdapt();
 });
+
