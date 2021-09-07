37fe3cd0731bd4338580ef5af833cdc00d27b46d
Tomas Orrego <tom.orrego@gmail.com>
2021-09-06 21:11:14 -0300

    avances 06 09 2021

--- src/js/index.js
+++ src/js/index.js
@@ -1,86 +1,60 @@
-let brand = document.getElementById("brand");
-let lineItem = Array.from(document.querySelectorAll('.nav-link'));
+let brand = document.querySelector('.navbar-brand img');
+let navLink = Array.from(document.querySelectorAll('.nav-link'));
+let navCollapse = document.querySelector('.navbar-collapse');
 let navbar = document.getElementById("navbar");
 let navToggler = document.getElementById("nav-toggler");
-let wWidth = window.innerWidth;
 
-function navAdapt() {
-    console.log(wWidth);
-    if (wWidth >= 768) {
-        window.addEventListener('scroll', function () {
-            scrollPosition = window.scrollY;
-            if (scrollPosition >= 30) {
-                navbar.classList.add("scrolled");
-                brand.src = "src/img/logo.svg";
-                lineItem.forEach((arrayElement, index) => {
-                    setTimeout(function () {
-                        lineItem[index].classList.add("text-grey");
-                        lineItem[index].classList.remove("text-white");
-                    }, 20);
-                });
-
-            } else {
-                navbar.classList.remove("scrolled");
-                brand.src = "src/img/logo-white.svg";
-                lineItem.forEach((arrayElement, index) => {
-                    setTimeout(function () {
-                        lineItem[index].classList.add("text-white");
-                        lineItem[index].classList.remove("text-grey");
-                    }, 20);
-                });
-            }
-
-        });
-    } else {
-        brand.src = "src/img/logo.svg";
-        lineItem.forEach((arrayElement, index) => {
-            setTimeout(function () {
-                lineItem[index].classList.add("text-grey");
-                lineItem[index].classList.remove("text-white");
-            }, 20);
-        });
-    }
-}
-
-function printWWidth() {
-    console.log(wWidth);
-}
-window.addEventListener('DOMContentLoaded', () => {
-    printWWidth();
-    navAdapt();
-})
-
-/* switch nav styles on scroll */
-window.addEventListener('resize', function () {
-    wWidth = window.innerWidth;
-    printWWidth();
-    navAdapt();
-});
-
-const svgContainer = document.getElementById('svg');
+// Lottie and BodyMovin
+const svgContainer = document.getElementById('hero-svg');
 const animItem = bodymovin.loadAnimation({
     wrapper: svgContainer,
     animType: 'svg',
     loop: false,
-    autoplay: true,
+    autoplay: false,
     path: 'src/img/anim-hero.json'
 });
 
+// Observers for animations
 const faders = document.querySelectorAll('.fade-in');
 const sliders = document.querySelectorAll('.slide-in');
+let hero = document.querySelector('.hero h1');
 
 const appearOptions = {
     threshold: 0,
     rootMargin: "0px 0px -150px 0px"
 };
 
-const appearOnScroll = new IntersectionObserver
-    (function (
+const sectionNavOptions = {
+    threshold: 0,
+    rootMargin: "-50px 0px 0px 0px"
+};
+const sectionNavObserver = new IntersectionObserver(function (
+        entries,
+        sectionNavObserver
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                navbar.classList.add("nav-scrolled");
+                brand.src = "src/img/logo.svg";
+                animItem.play();
+
+            } else {
+                navbar.classList.remove("nav-scrolled");
+                brand.src = "src/img/logo-white.svg";
+            }
+        })
+    },
+    sectionNavOptions);
+
+sectionNavObserver.observe(hero);
+
+const appearOnScroll = new IntersectionObserver(function (
         entries,
         appearOnScroll
     ) {
         entries.forEach(entry => {
             if (!entry.isIntersecting) {
+                entry.target.classList.remove("appear");
                 return;
             } else {
                 entry.target.classList.add("appear");
@@ -92,8 +66,23 @@ const appearOnScroll = new IntersectionObserver
 
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
-})
+});
 
 sliders.forEach(slider => {
     appearOnScroll.observe(slider);
-})
+});
+
+/* Nav behavior */
+
+navToggler.addEventListener('click', () => {
+    navbar.classList.add("nav-scrolled");
+    brand.src = "src/img/logo.svg";
+    navExpanded = true;
+
+});
+
+navLink.forEach(navLink => {
+    navLink.addEventListener('click', () => {
+        navCollapse.classList.remove('show');
+    });
+});
